942c41675f3d748a94cde7087cb1ab8d9f8f92b9
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-01-16 21:52:37 +0100

    added: special keys feature detection and removed c warnings

--- listenGlobalKeypress.c
+++ listenGlobalKeypress.c
@@ -67,6 +67,49 @@ char * charCopyFromCFString(CFStringRef aString) {
   return NULL;
 }
 
+// detect the special keys
+char * specialChar(CGKeyCode keycode) {
+
+  switch (keycode) {
+
+    case 36: return "RETURN";
+    case 48: return "TAB";
+    case 71: return "CLEAR";
+    case 49: return "SPACE";
+    case 51: return "DELETE";
+    case 52: return "ENTER";
+    case 53: return "ESCAPE";
+    case 96: return "F5";
+    case 97: return "F6";
+    case 98: return "F7";
+    case 99: return "F3";
+    case 100: return "F8";
+    case 101: return "F9";
+    case 103: return "F11";
+    case 105: return "F13";
+    case 107: return "F14";
+    case 109: return "F10";
+    case 111: return "F12";
+    case 113: return "F15";
+    case 114: return "HELP";
+    case 115: return "HOME";
+    case 116: return "PGUP";
+    case 117: return "DELETE";
+    case 118: return "F4";
+    case 119: return "END";
+    case 120: return "F2";
+    case 121: return "PGDN";
+    case 122: return "F1";
+    case 123: return "LEFT";
+    case 124: return "RIGHT";
+    case 125: return "DOWN";
+    case 126: return "UP";
+
+    default: return NULL;
+  }
+
+}
+
 
 /**
  * Callback function called whenever there is a keypress event
@@ -83,20 +126,24 @@ CGEventRef onKeyPress(
   if ((type != kCGEventKeyDown))
     return event;
 
-
   // The incoming keycode.
   CGKeyCode   keycode = (CGKeyCode) CGEventGetIntegerValueField(
                                     event,
                                     kCGKeyboardEventKeycode
                                   );
-  CFStringRef str     = CFStringFromCGKeyCode(keycode);
-  char *      utf8Str = charCopyFromCFString(str);
+  CFStringRef str;
+  char *      utf8Str = specialChar(keycode);
+
+  if (utf8Str == NULL) {
+    str     = CFStringFromCGKeyCode(keycode);
+    utf8Str = charCopyFromCFString(str);
+  }
 
   // go callback hook
   GoKeypressCallback(utf8Str);
 
-  // free the memory allocated to store this pointer
-  free(utf8Str);
+  // free the memory allocation
+  if (utf8Str == NULL) free(utf8Str);
 
   return event;
 
@@ -117,7 +164,7 @@ int listen() {
                 kCGHeadInsertEventTap,
                 0,
                 eventMask,
-                onKeyPress,
+                (CGEventTapCallBack) onKeyPress,
                 NULL
               );
 
